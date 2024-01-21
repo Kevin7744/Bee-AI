4150d3f0dac9ae3dacd421814914586f34db72d3
Kevin7744 <arapbiisubmissions@gmail.com>
2024-01-21 16:06:50 +0300

    Update

--- tools.py
+++ tools.py
@@ -1,4 +1,17 @@
 # Import the necessary packages
+import os
+from dotenv import load_dotenv, find_dotenv
+import openai
+
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from pydantic import BaseModel, Field
+from main import extract_payment_details
+
+load_dotenv(find_dotenv())
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+
 
 # Take user input
 
