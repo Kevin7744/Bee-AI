e91bdd9b01b6f185e180a76e909ab97d95228b7d
Kevin7744 <arapbiisubmissions@gmail.com>
2024-02-26 18:39:25 +0300

    Update

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from flask import Flask, request, jsonify
+
 import os
+
 from twilio.twiml.messaging_response import MessagingResponse
+
 from langchain.agents import initialize_agent, AgentType
 from langchain_community.chat_models import ChatOpenAI
+from langchain_mistralai.chat_models import ChatMistralAI
 from langchain.schema import SystemMessage
 from langchain.prompts import MessagesPlaceholder
 from langchain.memory import ConversationSummaryBufferMemory
+
 from Agent_Tools.tools import ExtractTillInformationTool, ExtractQrCodeInformationTool
+
 from Functions.Browsing.functions import SearchTool
 # from Functions.Apify.functions import CrawlWebsiteTool
 from Functions.Mpesa.till.functions import PaymentTillTool
 from Functions.Mpesa.paybill.functions import PaymentPaybillTool
 from Functions.Mpesa.qr_code.functions import QrCodeTool
+
+from Agent_Tools.Voice.tools import record_and_transcribe, chatgpt, print_colored, text_to_speech
+
 from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
 load_dotenv()
 
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+llm = ChatOpenAI(temperature=0)
 
 system_message = SystemMessage(content="""
     " You are Bee, a helpful Assistant!",
@@ -94,6 +104,13 @@ def chat():
 
     return str(twilio_resp)
 
+
+# while True:
+#     user_message = record_and_transcribe()
+#     response = chatgpt(api_key, conversation1, chatbot1, user_message)
+#     print_colored("Julie:", f"{response}\n\n")
+#     user_message_without_generate_image = re.sub(r'(Response:|Narration:|Image: generate_image:.*|)', '', response).strip()
+#     text_to_speech(user_message_without_generate_image, voice_id1, elapikey)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
